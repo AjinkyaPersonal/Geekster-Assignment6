b8423318c1d049420ae80bbf294587ac16c33630
Ajinkya <ajinkya12051999@gmail.com>
2023-04-12 01:36:34 +0530

    first commit of project

---
+++ src/test/java/com/geekster/fetch_Data_Using_Java/FetchDataUsingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.fetch_Data_Using_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchDataUsingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
